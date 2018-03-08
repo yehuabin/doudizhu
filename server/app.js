64e3c3be83b5eaad955d12fc1eeda0d0209d5b11
yehuabin <253922578@qq.com>
2018-03-09 07:53:45 +0800

    hello world

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const socket=require('socket.io');
 const app=socket('3000')
 app.on('connection',function(socket){
     console.log('a user connection');
-    socket.emit('welcome',"test");
+    socket.emit('welcome',"hello world");
     socket.on('login',function(data){
         console.log(`login : ${JSON.stringify(data)}`);
     })
